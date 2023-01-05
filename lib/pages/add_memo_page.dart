b983fe5458668ee73143db68c764889f3cab183d
shusuke919 <sh091965@gmail.com>
2023-01-06 01:08:14 +0900

    削除/編集のUI部分の作成

--- lib/pages/add_memo_page.dart
+++ lib/pages/add_memo_page.dart
@@ -1,35 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddMemoPage extends StatelessWidget {
+class AddMemoPage extends StatefulWidget {
   const AddMemoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddMemoPage> createState() => _AddMemoPageState();
+}
+
+class _AddMemoPageState extends State<AddMemoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController detailController = TextEditingController();
+
+  //メソットとして追加
+  Future<void> createMemo() async {
+    final memoCollection = FirebaseFirestore.instance.collection('memo');
+    await memoCollection.add({
+      'title' : titleController.text,
+      'detail' : detailController.text,
+      'createdDate' : Timestamp.now()
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('っっっd'),
+        title: const Text('っっっd'),
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 40),
-            Text('タイトル'),
+            const Text('タイトル'),
             const SizedBox(height: 20),
             Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey)
-              ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey)
+                ),
                 width: MediaQuery.of(context).size.width*0.8,
-            child: TextField(
-              decoration: InputDecoration(border: InputBorder.none,
-              contentPadding: const EdgeInsets.only(left: 10)
-              ),
-            )
+                child: TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10)
+                  ),
+                )
             ),
+            const SizedBox(height: 40),
+            const Text('詳細'),
+            const SizedBox(height: 20),
+            Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey)
+                ),
+                width: MediaQuery.of(context).size.width*0.8,
+                child: TextField(
+                  controller: detailController,
+                  decoration: InputDecoration(border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10)
+                  ),
+                )
+            ),
+            const SizedBox(height: 40),
+            Container(
+                width: MediaQuery.of(context).size.width*0.8,
+                alignment: Alignment.center,
+
+                child: ElevatedButton(
+                  onPressed: () async{
+                    //メモを処理する記述を作成
+                    await createMemo();
+                    //一番上のレイヤーを取り除く
+                    Navigator.pop(context);
+                  },
+                  child: const Text('追加'),
+                ))
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -16,29 +16,9 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
-List<Memo> memoList = [];
+ final memoCollection = FirebaseFirestore.instance.collection('memo');
 
-Future<void> fetchMemo() async{
-  //firebaseにアクセス
-  final memoCollection = await FirebaseFirestore.instance.collection('memo').get();
-  //memoコレクションを取得
-  final docs = memoCollection.docs;
-  for(var doc in docs) {
-    Memo fetchMemo = Memo(
-        title: doc.data()['title'],
-        detail: doc.data()['detail'],
-        createdDate: doc.data()['createdDate']
-    );
-   memoList.add(fetchMemo);
-  }
-  setState(() {});
-}
 
-@override
-void initState(){
-  super.initState();
-  fetchMemo();
-}
 
   @override
   Widget build(BuildContext context) {
@@ -47,19 +27,70 @@ void initState(){
 
         title: const Text('筋トレメモkk'),
       ),
-      body: ListView.builder(
-          itemCount: memoList.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(memoList[index].title),
-              //クリックできるようにするプロパティ
-              onTap: () {
-                //詳細画面に遷移する記述を書く
-                Navigator.push(context, MaterialPageRoute(
-                builder: (context) => MemoDetailPage(memoList[index])));
-              }
-            );
+      body: StreamBuilder<QuerySnapshot>(
+        stream: memoCollection.snapshots(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+    return const CircularProgressIndicator();
           }
+          // もしデータがなければ
+          if(!snapshot.hasData){
+            return const Center(child: Text('ないデータ'),);
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                // オブジェクト型はダイナミック型にする必要がある。ダイナミック型って何？
+                Map<String, dynamic> data = docs[index].data() as Map<String, dynamic>;
+
+                final Memo fetchMemo = Memo(
+                    title: data['title'],
+                    detail: data['detail'],
+                    createdDate: data['createdDate'],
+                    updatedDate: data['updatedDate']
+
+                );
+                return ListTile(
+                    title: Text(fetchMemo.title),
+                  trailing: IconButton(
+                    onPressed: (){
+                        showModalBottomSheet(context: context, builder: (context){
+                           return SafeArea(
+                             child: Column(
+                               mainAxisSize: MainAxisSize.min,
+                               children: const [
+                                 ListTile(
+                                   onTap: (){
+
+                                   },
+                                   leading: Icon(Icons.edit),
+                                   title: Text('編集'),
+                                 ),
+                                 ListTile(
+                                   onTap: {
+
+                                   },
+                                   leading: Icon(Icons.delete),
+                                   title: Text('削除'),
+                                 ),
+                               ],
+                             ),
+                           );
+                        });
+                    },
+                    icon: const Icon(Icons.edit),
+                  ),
+                  //クリックできるようにするプロパティ
+                  onTap: () {
+                    //詳細画面に遷移する記述を書く
+                    Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => MemoDetailPage(fetchMemo)));
+                  }
+                );
+              }
+          );
+        }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
